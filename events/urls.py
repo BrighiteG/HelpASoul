2413147a377a01d66dcd785374ea5d6b5ea2c6e2
Sergiu Manta <m.sergiu01@gmail.com>
2021-12-15 20:16:32 +0200

    created a basic Homepage with some navigation links, created the Homepage view function and configured the url path.

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from events import views
 
 urlpatterns = [
+    path('', views.HomeTemplateView.as_view(), name='homepage'),
     path('events_list', views.EventsListView.as_view(), name='events-list')
 ]
--- events/views.py
+++ events/views.py
@@ -4,6 +4,13 @@ from django.views.generic import ListView
 from events.models import Events
 
 
+from django.views.generic import TemplateView
+
+
+class HomeTemplateView(TemplateView):
+    template_name = 'base.html'
+
+
 class EventsListView(ListView):
     template_name = 'events/events_list.html'
     model = Events
