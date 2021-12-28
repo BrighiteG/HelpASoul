b91252b3c7e5ed6e850213326f3a59676862f067
Sergiu Manta <m.sergiu01@gmail.com>
2021-12-28 13:20:45 +0200

    Created the dynamic search query for Social Cases | Implemented the Social Case List-View |Changed the social_case_list_view from class based view to function based view| implemented social case detail view| created the searchbar for SocialCase | rendered the dynamic information for Social Cases|

--- social_cases/urls.py
+++ social_cases/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from social_cases import views
 
 urlpatterns = [
-    path('social_cases_list', views.SocialCasesListView.as_view(), name='social-cases'),
+    path('social_cases_list', views.social_case_list_view, name='social-cases'),
     path('social_case_update/<int:pk>/', views.SocialCaseUpdateView.as_view(), name='update-social-case'),
     path('social_case_create/', views.social_case_create, name='create-social-case'),
-    path('social_case_delete/<int:pk>/', views.SocialCaseDeleteView.as_view(), name='delete-social-case')
+    path('social_case_delete/<int:pk>/', views.SocialCaseDeleteView.as_view(), name='delete-social-case'),
+    path('social_case_detail/<int:pk>/', views.SocialCaseDetailView.as_view(), name='social_case_detail')
 ]
 
 
--- social_cases/views.py
+++ social_cases/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, UpdateView, CreateView, DeleteView
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView, DetailView
 
+from events.models import Tag
 from social_cases.forms import SocialCaseForm
 from social_cases.models import SocialCase
+from django.db.models import Q
 from users.models import Profile
 
 
@@ -22,10 +24,17 @@ def social_case_create(request):
     return render(request, 'social_cases/social_case_create.html', context)
 
 
-class SocialCasesListView(ListView):
-    template_name = 'social_cases/social_cases_list.html'
-    model = SocialCase
-    context_object_name = 'social_cases_list'
+def social_case_list_view(request):
+    search_query = ''
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+    tags = Tag.objects.filter(name__icontains=search_query)
+    social_cases = SocialCase.objects.distinct().filter(Q(title__icontains=search_query) |
+                                                        Q(description__icontains=search_query) |
+                                                        Q(case_tags__in=tags))
+
+    context = {'social_cases_list': social_cases, 'search_query': search_query}
+    return render(request, 'social_cases/social_cases_list.html', context)
 
 
 class SocialCaseUpdateView(UpdateView):
@@ -39,6 +48,10 @@ class SocialCaseDeleteView(DeleteView):
     template_name = 'social_cases/social_case_delete.html'
     model = SocialCase
     success_url = reverse_lazy('social-cases')
+
+class SocialCaseDetailView(DetailView):
+    template_name = 'social_cases/social_case_detail_view.html'
+    model = SocialCase
 
 
 
