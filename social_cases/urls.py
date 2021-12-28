24b3dce7827e8b17e0d67390f086fde08c9c3a85
Sergiu Manta <m.sergiu01@gmail.com>
2021-12-28 19:08:55 +0200

    Changed the logo | created the dynamic pagination| implemented the footer|

--- social_cases/urls.py
+++ social_cases/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from social_cases import views
 
 urlpatterns = [
-    path('social_cases_list', views.social_case_list_view, name='social-cases'),
+    path('social_cases_list/', views.social_case_list_view, name='social-cases'),
     path('social_case_update/<int:pk>/', views.SocialCaseUpdateView.as_view(), name='update-social-case'),
     path('social_case_create/', views.social_case_create, name='create-social-case'),
     path('social_case_delete/<int:pk>/', views.SocialCaseDeleteView.as_view(), name='delete-social-case'),
--- social_cases/views.py
+++ social_cases/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, UpdateView, CreateView, DeleteView, DetailView
-
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from events.models import Tag
 from social_cases.forms import SocialCaseForm
 from social_cases.models import SocialCase
@@ -32,8 +32,28 @@ def social_case_list_view(request):
     social_cases = SocialCase.objects.distinct().filter(Q(title__icontains=search_query) |
                                                         Q(description__icontains=search_query) |
                                                         Q(case_tags__in=tags))
+    page = request.GET.get('page')
+    results = 6
+    paginator = Paginator(social_cases, results)
+    try:
+        social_cases = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+    except EmptyPage:
+        page = paginator.num_pages
+        social_cases = paginator.page(page)
 
-    context = {'social_cases_list': social_cases, 'search_query': search_query}
+    left_index = (int(page) - 4)
+    if left_index < 1:
+        left_index = 1
+    right_index = (int(page) + 5)
+    if right_index > paginator.num_pages:
+        right_index = paginator.num_pages + 1
+
+
+    custom_range = range(left_index, right_index)
+
+    context = {'social_cases_list': social_cases, 'search_query': search_query, 'paginator': paginator, 'custom_range': custom_range}
     return render(request, 'social_cases/social_cases_list.html', context)
 
 
