b868ed2e3dd1dfb8136a1693ace065717428a8f5
Iancu Brighite <brighite@gmail.com>
2021-12-29 15:10:14 +0200

    Implemented the Social Case List-View |Changed the social_case_list_view from class based view to function based view

--- events/urls.py
+++ events/urls.py
@@ -4,8 +4,9 @@ from events import views
 
 urlpatterns = [
     path('', views.HomeTemplateView.as_view(), name='homepage'),
-    path('events_list/', views.EventsListView.as_view(), name='events-list'),
+    path('events_list/', views.event_list_view, name='events-list'),
     path('event_update/<int:pk>/', views.EventUpdateView.as_view(), name='event-update'),
     path('event_create/', views.event_create, name='event-create'),
-    path('event_delete/<int:pk>/', views.EventDeleteView.as_view(), name='event-delete')
+    path('event_delete/<int:pk>/', views.EventDeleteView.as_view(), name='event-delete'),
+    path('event_detail_view/<int:pk>/', views.EventDetailView.as_view(), name='event-detail-view')
 ]
