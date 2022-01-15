697d6b5c56327bd33f0e2d1e7ae789e4d82d657c
Iancu Brighite <brighite@gmail.com>
2022-01-15 20:20:57 +0200

    updated context processor with social_cases_with_percentages; homepage: updated section for social cases, blogs; events app: models.py: added 3 new model fields: location, gps coordinates long, gps coordinates lat, forms.py: updated create event form with new fields, templates: updated events list, details with new variables

--- HelpASoul/context_processors.py
+++ HelpASoul/context_processors.py
@@ -4,9 +4,14 @@ from social_cases.models import SocialCase
 
 def get_all_social_cases(request):
     social_cases = SocialCase.objects.all()
+    social_cases_with_percentages = []
+    for social_case in social_cases:
+        percent = social_case.percent_raised()
+        amount_raised = social_case.total_donations()
+        social_cases_with_percentages.append({'social_case': social_case, 'percent': percent,
+                                              'amount_raised': amount_raised})
 
-
-    return {'social_cases': social_cases}
+    return {'social_cases': social_cases, 'social_cases_with_percentages': social_cases_with_percentages}
 
 
 def get_all_blog_posts(request):
--- events/models.py
+++ events/models.py
@@ -14,12 +14,15 @@ class Tag(models.Model):
 class Event(models.Model):
     title = models.CharField(max_length=200)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    organizer = models.CharField(max_length=200)
+    organisation = models.CharField(max_length=200, null=True, blank=False)
     description = models.TextField(null=True, blank=False)
     created = models.DateTimeField(auto_now_add=True)
     profile_image = models.ImageField(upload_to='static/images/dynamic', default='static/images/Aesthetic-Desktop-Wallpaper.jpg')
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
+    location = models.CharField(max_length=200, null=True, blank=False)
+    gps_coordinates_long = models.DecimalField(max_digits=20, decimal_places=8, default=10.00000000)
+    gps_coordinates_lat = models.DecimalField(max_digits=20, decimal_places=8, default=10.00000000)
     event_tags = models.ManyToManyField(Tag)
 
     def __str__(self):
