598cc176d5090fa4e94805f31bfe66763b6851d2
Manta Sergiu Stefan <m.sergiu01@gmail.com>
2022-01-26 16:16:17 +0200

    implemented newsletter feature

--- HelpASoul/settings.py
+++ HelpASoul/settings.py
@@ -136,8 +136,8 @@ LOGOUT_REDIRECT_URL = '/login/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 # EMAIL_HOST_USER = os.environ.get('gmail_user')
-EMAIL_HOST_USER = 'helpasoul24@gmail.com'
-EMAIL_HOST_PASSWORD = 'mantasergiu1994'
+EMAIL_HOST_USER = os.environ.get('gmail_user')
+EMAIL_HOST_PASSWORD = os.environ.get('gmail_password')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
